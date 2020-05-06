350fdaf1f23481570efc86e722179372b3f8da46
Brandon Li <brandonl7ce@gmail.com>
2020-05-06 16:35:02 -0400

    asm1 reupload again

--- asm1/asm1/runantlr.bat
+++
@@ -1,3 +0,0 @@
-set ANTLR="C:\Users\Brandon Li\source\repos\BrandonL7ce\antlrJar\antlr-4.8-complete.jar"
-set JAVA="C:\Program Files (x86)\Java\jre1.8.0_161\bin\java.exe"
-%JAVA% -cp %ANTLR% org.antlr.v4.Tool -Dlanguage=JavaScript "%1"
